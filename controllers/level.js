0e9279aba0f9a4d27f8c018b6370b18e964c0dca
Bwana Kizito <kizomanizo@gmail.com>
2020-01-30 16:50:51 +0300

    Verbose JSON responses on Level controler.

--- controllers/level.js
+++ controllers/level.js
@@ -5,7 +5,11 @@ module.exports = {
     // List all levels stored
     all(_req, res) {
         Level.findAll()
-        .then(levels =>  res.status(200).send(levels))
+        .then(levels =>  res.status(200).send({
+            success: true,
+            message: "All levels retrieved",
+            entity: levels,
+        }))
         .catch(error => res.status(400).send(error));
     },
 
@@ -16,22 +20,34 @@ module.exports = {
             name:       req.body.name,
             created_by: req.body.user,
         })
-        .then(level => res.status(201).send(level))
+        .then(level => res.status(201).send({
+            success: true,
+            message: "Level creaded successfully",
+            entity: level,
+        }))
         .catch(error => res.status(400).send(error));
     },
     
     // Search for a specific level using UUID, for Primary key utumie findByPk(req.params.LevelId)
     search(req, res) {
         Level.findOne({where: {uuid: req.params.LevelUuid} })
-        .then(level =>  res.status(200).send(level))
+        .then(level =>  res.status(200).send({
+            success: true,
+            message: "Specific level retrieved",
+            entity: level,
+        }))
         .catch(error => res.status(400).send(error));
     },
 
     // Delete a specific level using UUID, for Primary key utumie findByPk(req.params.LevelId)
     delete(req, res) {
-      Level.findOne({where: {uuid: req.params.LevelUuid} })
-      .then(level => { return level.destroy()} )
-      .then(level =>  res.status(200).send(level))
-      .catch(error => res.status(400).send(error));
+        Level.findOne({where: {uuid: req.params.LevelUuid} })
+        .then(level => { return level.destroy()} )
+        .then(level =>  res.status(200).send({
+            success: true,
+            message: "Level deleted successfully",
+            entity: level,
+        }))
+        .catch(error => res.status(400).send(error));
     },
 };
